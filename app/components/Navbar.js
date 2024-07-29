9d4892f19502e674d7b36093740266c8f11c0e31
apskahlon15 <armanpreetsinghk@gmail.com>
2024-07-28 21:57:15 -0600

    Worked on Herobanner

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Logo from "../assets/images/Logo.png";
+import Image from "next/image"; // Import the Image component
 
 const Navbar = () => (
   <div className="bg-white flex items-center px-5 ">
     <a href="/" className="flex items-center mr-10 mt-7  ">
-      <img
+      <Image
         src={Logo}
         alt="logo"
-        className="w-12 h-12 ml-4 mr-16" // Tailwind classes for width and height
+        className="w-12 h-12 ml-4 mr-16 pb-2" // Tailwind classes for width and height
       />
     </a>
     <div className="flex gap-10 font-serif text-xl items-end">
